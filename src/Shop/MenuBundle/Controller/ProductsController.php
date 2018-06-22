5c046ef9d22347a6b0e7396e78a558f5f465ab28
andriy202ip2 <andriy202ip2@gmail.com>
2018-06-22 05:22:33 +0300

    add oll prodacts user

--- src/Shop/MenuBundle/Controller/ProductsController.php
+++ src/Shop/MenuBundle/Controller/ProductsController.php
@@ -4,62 +4,70 @@ namespace Shop\MenuBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Money\Money;
+use Money\Currency;
 
-class ProductsController extends Controller {
+class ProductsController extends Controller
+{
 
-    public function indexAction() {
+    public function indexAction()
+    {
 
         $em = $this->getDoctrine()->getManager();
         $modelMenus = $em->getRepository('ShopMenuBundle:ModelMenu')
-                ->findAllOrderedByName();
+            ->findAllOrderedByName();
 
         $Items = $em->getRepository('ShopMenuBundle:Items')
             ->findTheLatestOnes(30);
 
         return $this->render('ShopMenuBundle:Products:index.html.twig', array(
-                    'modelMenus' => $modelMenus,
-                    'Items' => $Items,
+            'modelMenus' => $modelMenus,
+            'Items' => $Items,
         ));
     }
 
-    public function autosAction(Request $request, $model_id) {
+    public function autosAction(Request $request, $model_id)
+    {
 
         $em = $this->getDoctrine()->getManager();
         $autoMenu = $em->getRepository('ShopMenuBundle:AutoMenu')
-                ->findByIdOrderedByName($model_id);
+            ->findByIdOrderedByName($model_id);
 
         return $this->render('ShopMenuBundle:Products:auto.html.twig', array(
-                    'autoMenu' => $autoMenu,
+            'autoMenu' => $autoMenu,
         ));
     }
 
-    public function dataAction($model_id, $auto_id) {
+    public function dataAction($model_id, $auto_id)
+    {
 
         $em = $this->getDoctrine()->getManager();
         $dataMenu = $em->getRepository('ShopMenuBundle:DataMenu')
-                ->findByIdOrderedByName($model_id, $auto_id);
+            ->findByIdOrderedByName($model_id, $auto_id);
 
         return $this->render('ShopMenuBundle:Products:data.html.twig', array(
-                    'dataMenu' => $dataMenu,
+            'dataMenu' => $dataMenu,
         ));
     }
 
-    public function itemsAction($model_id, $auto_id, $data_id) {
-        
+    public function itemsAction($model_id, $auto_id, $data_id)
+    {
+
         $em = $this->getDoctrine()->getManager();
         $ItemsArray = $em->getRepository('ShopMenuBundle:Items')
-                ->findByIdOrderedById($model_id, $auto_id, $data_id, 0);
+            ->findByIdOrderedById($model_id, $auto_id, $data_id, 0);
 
         return $this->render('ShopMenuBundle:Products:items.html.twig', array(
-                    'ItemsArray' => $ItemsArray,
+            'ItemsArray' => $ItemsArray,
         ));
     }
 
-    public function itemAction($product_id) {
+    public function itemAction($product_id)
+    {
 
         $em = $this->getDoctrine()->getManager();
         //$Item = $em->getRepository('ShopMenuBundle:Items')
-            //->getOneItemById($product_id);
+        //->getOneItemById($product_id);
 
         $Item = $em->getRepository('ShopMenuBundle:Items')->findOneBy(["id" => $product_id]);
 
@@ -74,6 +82,81 @@ class ProductsController extends Controller {
         return $this->render('ShopMenuBundle:Products:item.html.twig', array(
             'Item' => $Item,
             'KodProdakt' => $KodProdakt,
+        ));
+    }
+
+    public function allAction(Request $request)
+    {
+
+        $page = $request->query->getInt('page', 1);
+        $isGal = $request->query->getInt('isgal', 0);
+        $currency = $request->query->getInt('currency', 0);
+        $sorts = $request->query->getInt('sorts', 0);
+
+        $em = $this->getDoctrine()->getManager();
+        //$Item = $em->getRepository('ShopMenuBundle:Items')
+        //->getOneItemById($product_id);
+
+        $pairManager = $this->get('tbbc_money.pair_manager');
+        $USD = $pairManager->convert(Money::USD(100), 'UAH')->getAmount() / 100;
+        $EUR = $pairManager->convert(Money::EUR(100), 'UAH')->getAmount() / 100;
+        $PLN = $pairManager->convert(Money::PLN(100), 'UAH')->getAmount() / 100;
+
+        //echo $USD.'<br>';
+        //echo $EUR;
+
+        $dql = $em->getRepository('ShopMenuBundle:Items')
+            ->createQueryBuilder('a');
+
+        $case_str = "case when a.priceCurrency = 'USD' then :USD else 
+                     case when a.priceCurrency = 'EUR' then :EUR else 
+                     case when a.priceCurrency = 'PLN' then :PLN else 1 end end end";
+
+        if ($sorts == 0) {
+
+            $dql = $dql->orderBy('a.id', 'DESC');
+
+            //chipest
+        } elseif ($sorts == 1) {
+
+            $dql = $dql->where('a.priceAmount > 0');
+            $dql = $dql->orderBy('a.priceAmount * '.$case_str , 'ASC');
+
+            $dql = $dql->setParameter(':USD', $USD)
+                        ->setParameter(':EUR', $EUR)
+                        ->setParameter(':PLN', $PLN);
+
+            //expensive
+        } else {
+
+            $dql = $dql->where('a.priceAmount > 0');
+            $dql = $dql->orderBy('a.priceAmount * '.$case_str , 'DESC');
+
+            $dql = $dql->setParameter(':USD', $USD)
+                ->setParameter(':EUR', $EUR)
+                ->setParameter(':PLN', $PLN);
+        }
+
+        $query = $dql->getQuery();
+
+        $paginator = $this->get('knp_paginator');
+
+        $MaxPagas = 30;
+        $Items = $paginator->paginate(
+            $query, /* query NOT result */
+            $request->query->getInt('page', 1)/* page number */, $MaxPagas/* limit per page */
+        );
+
+
+        //var_dump($request->query->all());
+
+        return $this->render('ShopMenuBundle:Products:all.html.twig', array(
+            'Items' => $Items,
+            'MaxPagas' => $MaxPagas,
+            'page' => $page,
+            'isGal' => $isGal,
+            'currency' => $currency,
+            'sorts' => $sorts,
         ));
     }
 
